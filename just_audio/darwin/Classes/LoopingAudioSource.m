6a3d20e8ea52ec1ecc02cb7d19bf00310d38b702
Sam Smets <sam.smets@nix-digital.com>
2024-06-27 18:34:34 +0800

    Fixed issue with looping audio source

--- just_audio/darwin/Classes/LoopingAudioSource.m
+++ just_audio/darwin/Classes/LoopingAudioSource.m
@@ -1,13 +1,12 @@
-#import "AudioSource.h"
 #import "LoopingAudioSource.h"
+#import "AudioSource.h"
 #import <AVFoundation/AVFoundation.h>
 
 @implementation LoopingAudioSource {
-    // An array of duplicated audio sources
-    NSArray<AudioSource *> *_audioSources;
+    // An array of duplicates
+    NSArray<AudioSource *> *_audioSources; // <AudioSource *>
 }
 
-// Initialize the LoopingAudioSource with an array of duplicated audio sources
 - (instancetype)initWithId:(NSString *)sid audioSources:(NSArray<AudioSource *> *)audioSources {
     self = [super initWithId:sid];
     NSAssert(self, @"super init cannot be nil");
@@ -15,19 +14,16 @@
     return self;
 }
 
-// Check if lazy loading is enabled for the audio sources
 - (BOOL)lazyLoading {
     return [_audioSources count] > 0 ? _audioSources[0].lazyLoading : NO;
 }
 
-// Set lazy loading for all audio sources
 - (void)setLazyLoading:(BOOL)lazyLoading {
     for (int i = 0; i < [_audioSources count]; i++) {
         _audioSources[i].lazyLoading = lazyLoading;
     }
 }
 
-// Build the sequence of audio sources
 - (int)buildSequence:(NSMutableArray *)sequence treeIndex:(int)treeIndex {
     for (int i = 0; i < [_audioSources count]; i++) {
         treeIndex = [_audioSources[i] buildSequence:sequence treeIndex:treeIndex];
@@ -35,15 +31,13 @@
     return treeIndex;
 }
 
-// Find audio sources by their ID and add matches to the matches array
 - (void)findById:(NSString *)sourceId matches:(NSMutableArray<AudioSource *> *)matches {
-    [super.findById:sourceId matches:matches];
+    [super findById:sourceId matches:matches];
     for (int i = 0; i < [_audioSources count]; i++) {
         [_audioSources[i] findById:sourceId matches:matches];
     }
 }
 
-// Get the shuffle indices for the audio sources
 - (NSArray<NSNumber *> *)getShuffleIndices {
     NSMutableArray<NSNumber *> *order = [NSMutableArray new];
     int offset = (int)[order count];
@@ -58,7 +52,6 @@
     return order;
 }
 
-// Decode the shuffle order from a dictionary
 - (void)decodeShuffleOrder:(NSDictionary *)dict {
     NSDictionary *dictChild = (NSDictionary *)dict[@"child"];
     for (int i = 0; i < [_audioSources count]; i++) {
